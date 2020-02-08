b721a8b06ee0483a836b194f68f840f26d18952e
Ali Maleki <ali.maleki100@gmail.com>
2020-02-09 02:29:39 +0330

    Create JoinTemplate.sql

---
+++ JoinTemplate.sql
@@ -0,0 +1,20 @@
+--Inner Join
+
+Select t1.column1,
+t2.column1
+FROM table1 as t1
+	INNER JOIN table2 as t2
+	on t1.column1 = t2.column1
+
+
+
+	--Outer Join
+
+Select t1.column1,
+t2.column1
+FROM table1 as t1
+	 JOIN table2 as t2
+	on t1.column1 = t2.column1
+
+
+
