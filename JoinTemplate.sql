85ebfd5f37d2752f205605c06f99b2d6f0e2b9a4
Ali Maleki <ali.maleki100@gmail.com>
2020-02-11 03:42:29 +0330

    2

--- JoinTemplate.sql
+++ JoinTemplate.sql
@@ -17,4 +17,15 @@ FROM table1 as t1
 	on t1.column1 = t2.column1
 
 
+	-- Multiple Joins
+
+	Select t1.column1,
+t2.column1,
+t3.column1
+FROM table1 as t1
+	 JOIN table2 as t2
+	on t1.column1 = t2.column1
+	join table3 as t3 
+	on t2.column2=t3.column2
+
 
