cb90036fce34e7442404aeb391365d08520bb3bf
Ali Maleki <ali.maleki100@gmail.com>
2020-02-12 05:40:29 +0330

    convertData

---
+++ ConvertDataTypes.sql
@@ -0,0 +1,14 @@
+convert(newDataType,column1)
+
+cast =(column1 AS newDataType)
+
+
+--convert for datetime styles
+convert(varchar,datetimeColumn,datetimestyle(0..100....101...))
+
+
+--convert for money styles
+convert(varchar,moneyColumn,moneystyle(0/1/2))
+
+
+round(column1,precision,truncate or not(0/1))
